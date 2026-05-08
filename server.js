d3668faa08d07b635520640d0e78e32b4a78ce3b
Kartik Sharma <kartiksharma200029@gmail.com>
2026-05-08 20:23:05 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const dotenv = require("dotenv");
+
+const app = require("./app");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
